f2f24ec7cf8f9d0b0ab1c636731f167d4a0e8021
syedmohin <syedmohin@users.noreply.github.com>
2020-02-23 16:13:21 +0530

    Goku Amry

--- src/main/java/com/sunday/AhmedMailController.java
+++ src/main/java/com/sunday/AhmedMailController.java
@@ -2,16 +2,18 @@ package com.sunday;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Paths;
 import java.util.List;
@@ -22,16 +24,15 @@ import java.util.List;
 public class AhmedMailController {
 
     private final AhmedMailService service;
+    private final Repo repo;
 
     @RequestMapping("/getdata")
-    public String confirm(HttpServletRequest request) {
-        String userName = request.getParameter("name");
-        String mail = request.getParameter("mail");
-        String msg = request.getParameter("msg");
-        String sub = request.getParameter("sub");
+    public String confirm(@RequestParam("name") String userName, @RequestParam("mail") String mail, @RequestParam("msg") String msg, @RequestParam("sub") String sub) {
         UserData data = new UserData(userName, mail, msg, sub);
-        log.info("Data recevied from User {}", userName);
         service.sendNotification(data);
+        log.info("Data recevied from User {}", userName);
+        repo.save(data);
+        log.info("Data recevied from User {}", userName);
         return "details";
     }
 
@@ -51,26 +52,40 @@ public class AhmedMailController {
 
     @ResponseBody
     @RequestMapping("/getresume")
-    public void downloadFile(HttpServletResponse response) {
+    public ResponseEntity<?> downloadFile(HttpServletResponse response) {
 //        Resource resource = new ClassPathResource("resume.pdf");
         String path = String.valueOf(Paths.get("").toAbsolutePath());
-        log.info(path+"/resume.pdf");
+//        URL path = AhmedMailController.class.getResource("resume.pdf");
+
+        File f = new File("D:\\Programs\\Spring\\Ahmed\\src\\main\\resources\\resume.pdf");
+//        File f = new File("https://drive.google.com/open?id=1Fh24_ojO2LySGWAdm1EnT05kAo-99jHq");
+        log.info(f.getAbsolutePath());
         response.setContentType("application/pdf");
         response.setHeader("Content-Disposition", "attachment; filename=resume.pdf");
         response.setHeader("Content-Transfer-Encoding", "binary");
+        BufferedOutputStream bufferedOutputStream = null;
         try {
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(response.getOutputStream());
-            FileInputStream fileInputStream = new FileInputStream(path+"\\resume.pdf");
+            bufferedOutputStream = new BufferedOutputStream(response.getOutputStream());
+            FileInputStream fileInputStream = new FileInputStream(f);
             int len;
             byte[] buf = new byte[1024];
             while ((len = fileInputStream.read(buf)) > 0) {
                 bufferedOutputStream.write(buf, 0, len);
             }
-            bufferedOutputStream.close();
-            response.flushBuffer();
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType("application/pdf"))
+                    .body(bufferedOutputStream);
         } catch (Exception e) {
-            log.error("Unable to download file  {} ",e.getMessage());
+            log.error("Unable to download file  {} ", e.toString());
+        } finally {
+            try {
+                if (bufferedOutputStream != null) bufferedOutputStream.close();
+                response.flushBuffer();
+            } catch (Exception e) {
+                log.error("Unable to download file  {} ", e.getMessage());
+            }
         }
+        return new ResponseEntity<>("error ouccred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ResponseBody
